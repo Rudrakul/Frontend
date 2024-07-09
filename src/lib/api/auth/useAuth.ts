a030d1296b8a0810a4d9518dc734879e24c5af9d
Utkarsh Mishra <utkarshmishra2001@gmail.com>
2024-07-09 22:01:21 +0530

    FEAT: Abstracted api calls

---
+++ src/lib/api/auth/useAuth.ts
@@ -0,0 +1,52 @@
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { login, logout, register, requestOtp, verifyOtp, resetPassword } from './endpoints';
+import type { AuthRequest, OtpRequest } from '@/types/auth';
+
+// Queries
+export const useLogout = () => {
+    return useQuery('auth', logout);
+};
+
+export const useRequestOtp = (email: string) => {
+    return useQuery('auth', () => requestOtp(email));
+}
+
+
+// Mutations
+const queryClient = useQueryClient();
+
+export const useLogin = (requestBody: AuthRequest) => {
+
+    return useMutation((requestBody: AuthRequest) => login(requestBody), {
+        onSuccess: () => {
+            queryClient.invalidateQueries(['auth', 'user']);
+        },
+    });
+};
+
+export const useRegister = (requestBody: AuthRequest) => {
+
+    return useMutation((requestBody: AuthRequest) => register(requestBody), {
+        onSuccess: () => {
+            queryClient.invalidateQueries(['auth', 'user']);
+        },
+    });
+};
+
+export const useVerifyOtp = (requestBody: OtpRequest) => {
+
+    return useMutation((requestBody: OtpRequest) => verifyOtp(requestBody), {
+        onSuccess: () => {
+            queryClient.invalidateQueries(['auth', 'user']);
+        },
+    });
+};
+
+export const useResetPassword = (requestBody: AuthRequest) => {
+
+    return useMutation((requestBody: AuthRequest) => resetPassword(requestBody), {
+        onSuccess: () => {
+            queryClient.invalidateQueries(['auth', 'user']);
+        },
+    });
+};
